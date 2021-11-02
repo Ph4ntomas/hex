a082e86c97ec266dfe96f4311b0914db9842e4c7
Phantomas <phantomas@phantomas.xyz>
2021-11-02 08:36:43 +0100

    add: init_test sparse_array
    CHANGELOG:
    - Changes how initializer_list are handled.
    - add CTAD guides for iterator based ctor.
    - Fix various bugs & bad SFINAE
    - Unit Test all function and most necessary functions.

--- srcs/hex/containers/sparse_array.hpp
+++ srcs/hex/containers/sparse_array.hpp
@@ -3,7 +3,7 @@
 **
 ** \author Phantomas <phantomas@phantomas.xyz>
 ** \date Created on: 2021-10-29 12:28
-** \date Last update: 2021-10-31 17:04
+** \date Last update: 2021-11-11 23:53
 */
 
 #ifndef SPARSE_ARRAY_HPP_
@@ -15,9 +15,29 @@
 #include <stdexcept>
 #include <vector>
 
+namespace hex::meta {
+    template <class Iter>
+    using iterator_category_t = typename std::iterator_traits<Iter>::iterator_category;
+
+    template <class Iter>
+    using require_input_iterator = std::enable_if_t<std::is_convertible_v<iterator_category_t<Iter>, std::input_iterator_tag>>;
+
+    template <typename>
+    struct is_optional : std::false_type {};
+
+    template <typename T>
+    struct is_optional<std::optional<T>> : std::true_type {};
+
+    template <typename T>
+    static constexpr bool is_optional_v = is_optional<T>::value;
+}
+
 namespace hex::containers {
+
     template <typename T, typename Allocator = std::allocator<std::optional<T>>>
     class sparse_array : std::vector<std::optional<T>, Allocator> {
+        static_assert(!std::is_same_v<T, std::nullopt_t>, "Component type cannot be std::nullopt_t");
+
         using base_t = std::vector<std::optional<T>, Allocator>;
         public:
             using value_type = typename base_t::value_type;
@@ -35,12 +55,32 @@ namespace hex::containers {
             using base_t::base_t;
             //sparse_array() noexcept(noexcept(Allocator()));
             //explicit sparse_array(Allocator const & alloc) noexcept;
-            template <typename U = T>
-            sparse_array(size_type count, U &&value, Allocator const &alloc = Allocator()) : base_t(count, std::optional(std::forward<U>(value)), alloc) {}
+            template <typename U = T, std::enable_if_t<!
+                std::disjunction_v<
+                    std::is_same<std::nullopt_t, std::decay_t<U>>,
+                    std::is_same<std::allocator<value_type>, std::decay_t<U>>,
+                    std::is_same<U, value_type>,
+                    std::is_constructible<value_type, U&&>
+                >, bool> = true>
+                sparse_array(size_type count, U const & value, Allocator const &alloc = Allocator())
+                : base_t(count, std::make_optional<std::decay_t<U>>(value), alloc) {}
+
             //explicit sparse_array(size_type count, Allocator const &alloc = Allocator());
 
-            //template <class InputIt>
-            //sparse_array(InputIt first, InputIt last, Allocator const &alloc = Allocator());
+            template <class InputIt, std::enable_if_t<!
+                std::disjunction_v<
+                    std::is_same<std::nullopt_t, typename std::iterator_traits<InputIt>::value_type>,
+                    std::is_same<value_type, typename std::iterator_traits<InputIt>::value_type>,
+                    std::is_constructible<value_type, typename std::iterator_traits<InputIt>::value_type>
+                >, bool> = true, typename = meta::require_input_iterator<InputIt>>
+            sparse_array(InputIt first, InputIt last, Allocator const &alloc = Allocator()) : base_t(last - first, alloc) {
+                std::transform(first, last, begin(), [](auto const &t){ return std::make_optional<T>(t); });
+            }
+
+            template <class InputIt, typename = std::enable_if_t<
+                meta::is_optional_v<typename std::iterator_traits<InputIt>::value_type>>,
+                     typename = meta::require_input_iterator<InputIt>>
+            sparse_array(InputIt first, InputIt last, Allocator const &alloc = Allocator()) : base_t{first, last, alloc} {}
 
             //sparse_array(sparse_array const & oth) : base_t(oth) {}
             //sparse_array(sparse_array const &, Allocator const &alloc) : base_t(
@@ -49,13 +89,18 @@ namespace hex::containers {
             //sparse_array(sparse_array &&) noexcept;
             //sparse_array(sparse_array &&, Allocator const & alloc);
 
-            sparse_array(std::initializer_list<T> init, Allocator const &alloc = Allocator()) : base_t(init.size(), alloc) {
-                if constexpr (std::is_move_constructible_v<T>)
-                    std::transform(init.begin(), init.end(), begin(), [](auto &&t){ return std::optional(std::move(t)); });
-                else {
-                    std::transform(init.begin(), init.end(), begin(), [](auto const &t){ return std::optional(t); });
-                }
+
+            sparse_array(std::initializer_list<value_type> init, Allocator const &alloc = Allocator()) : base_t(init, alloc) {}
+
+            template <typename U = T, std::enable_if_t<!
+                std::disjunction_v<
+                    std::is_same<U, value_type>,
+                    std::is_constructible<value_type, U&&>
+                >, bool> = true>
+            sparse_array(std::initializer_list<U> init, Allocator const &alloc = Allocator()) : base_t(init.size(), alloc) {
+                std::transform(init.begin(), init.end(), begin(), [](auto const &t){ return std::make_optional<T>(t); });
             }
+
             //sparse_array(std::initializer_list<value_type> init, Allocator const &alloc = Allocator());
 
             //~sparse_array();
@@ -64,18 +109,63 @@ namespace hex::containers {
             //sparse_array &operator=(sparse_array const &);
             //sparse_array &operator=(sparse_array &&) noexcept;
 
+            sparse_array &operator=(std::initializer_list<value_type> init) { return reinterpret_cast<sparse_array &>(base_t::operator=(init)); }
+
+            template <typename U = T, std::enable_if_t<!
+                std::disjunction_v<
+                    std::is_same<std::nullopt_t, U>,
+                    std::is_same<U, value_type>,
+                    std::is_constructible<value_type, U&&>
+                >, bool> = true>
+            sparse_array &operator=(std::initializer_list<U> init) {
+                assign(init);
+                return *this;
+            }
+
             using base_t::assign;
             //void assign(size_t count, T const &value);
-            //template <class InputIt>
-            //void assign(InputIt first, InputIt last);
-            void assign(std::initializer_list<T> init) {
-                resize(init.size());
 
-                if constexpr (std::is_move_constructible_v<T>)
-                    std::transform(init.begin(), init.end(), begin(), [](auto &&t){ return std::optional(std::move(t)); });
-                else {
-                    std::transform(init.begin(), init.end(), begin(), [](auto const &t){ return std::optional(t); });
+            template <typename U = T, std::enable_if_t<!
+                std::disjunction_v<
+                    std::is_same<std::nullopt_t, std::decay_t<U>>,
+                    std::is_same<U, value_type>,
+                    std::is_constructible<value_type, U&&>
+                >, bool> = true>
+                void assign(size_t count, U const &v) {
+                     base_t::assign(count, std::make_optional<std::decay_t<U>>(v));
                 }
+
+            template <class InputIt, std::enable_if_t<!
+                std::disjunction_v<
+                    std::is_same<std::nullopt_t, typename std::iterator_traits<InputIt>::value_type>,
+                    std::is_same<value_type, typename std::iterator_traits<InputIt>::value_type>,
+                    std::is_constructible<value_type, typename std::iterator_traits<InputIt>::value_type>
+                >, bool> = true, typename = meta::require_input_iterator<InputIt>>
+            void assign(InputIt first, InputIt last) {
+                size_t dist = last - first;
+
+                base_t::resize(dist);
+
+                std::transform(first, last, base_t::begin(), [](auto &t){ return std::make_optional<T>(t); });
+            }
+
+            template <class InputIt, typename = std::enable_if_t<
+                meta::is_optional_v<typename std::iterator_traits<InputIt>::value_type>>,
+                     typename = meta::require_input_iterator<InputIt>>
+            void assign(InputIt first, InputIt last) { base_t::assign(first, last); }
+
+            void assign(std::initializer_list<value_type> init) { base_t::assign(init); }
+
+            template <typename U = T, std::enable_if_t<!
+                std::disjunction_v<
+                    std::is_same<std::nullopt_t, U>,
+                    std::is_same<U, value_type>,
+                    std::is_constructible<value_type, U&&>
+                >, bool> = true>
+            void assign(std::initializer_list<U> init) {
+                base_t::resize(init.size());
+
+                std::transform(init.begin(), init.end(), begin(), [](auto const &t){ return std::make_optional<T>(t); });
             }
 
             using base_t::get_allocator;
@@ -97,8 +187,14 @@ namespace hex::containers {
             //[[nodiscard]] pointer data() noexcept;
             //[[nodiscard]] const_pointer data() const noexcept;
 
-            [[nodiscard]] size_type get_index(T const &t) {
-                auto diff = std::addressof(t) - data();
+            template <typename U = T, typename = std::enable_if_t<
+                std::disjunction_v<
+                    std::is_same<std::decay_t<U>, T>,
+                    std::is_same<std::decay_t<U>, value_type>
+                >
+            >>
+            [[nodiscard]] size_type get_index(U const &t) const {
+                auto diff = (value_type *)std::addressof(t) - data();
 
                 if (diff < 0 || diff > size())
                     throw std::out_of_range("get_index error: components does not exists in sparse_array.");
@@ -154,19 +250,19 @@ namespace hex::containers {
             **/
             using base_t::clear;
             //void clear() noexcept;
-            reference insert_at(size_type pos, T const & value) {
+
+            template <typename U = T>
+            iterator insert_at(size_type pos, U && value) {
                 maybe_resize(pos);
 
-                return base_t::at(pos) = value;
+                base_t::at(pos) = std::forward<U>(value);
+                return base_t::begin() + pos;
             }
 
-            reference insert_at(size_type pos, T && value) {
-                maybe_resize(pos);
-
-                return base_t::at(pos) = std::forward<T>(value);
-            }
-
-            template <class... Args> iterator emplace_at(size_type pos, Args &&... args) {
+            template <class... Args, typename = std::enable_if_t<
+                std::is_constructible_v<value_type, Args...>
+            >>
+            iterator emplace_at(size_type pos, Args &&... args) {
                 maybe_resize(pos);
                 auto alloc = get_allocator();
 
@@ -177,29 +273,101 @@ namespace hex::containers {
                 traits_t::destroy(alloc, addr);
                 traits_t::construct(alloc, addr, std::forward<Args>(args)...);
 
+                return base_t::begin() + pos;
             }
 
-            void erase_at(const_iterator pos) {
+            template <class... Args, std::enable_if_t<
+                !std::is_constructible_v<value_type, Args...>, bool
+            > = true>
+            iterator emplace_at(size_type pos, Args &&... args) {
+                maybe_resize(pos);
+                auto alloc = get_allocator();
+
+                using traits_t = std::allocator_traits<decltype(alloc)>;
+
+                auto addr = data() + pos;
+
+                traits_t::destroy(alloc, addr);
+                traits_t::construct(alloc, addr, std::in_place, std::forward<Args>(args)...);
+
+                return base_t::begin() + pos;
+            }
+
+            void erase_at(size_type pos) {
                 at(pos) = std::nullopt;
             }
 
+            using base_t::resize;
             using base_t::swap;
             /**
             ** \@}
             */
 
-            friend bool operator==(sparse_array<T, Allocator> const &lhs, sparse_array<T, Allocator> const &rhs);
-            friend bool operator!=(sparse_array<T, Allocator> const &lhs, sparse_array<T, Allocator> const &rhs);
-            friend bool operator<(sparse_array<T, Allocator> const &lhs, sparse_array<T, Allocator> const &rhs);
-            friend bool operator>(sparse_array<T, Allocator> const &lhs, sparse_array<T, Allocator> const &rhs);
-            friend bool operator<=(sparse_array<T, Allocator> const &lhs, sparse_array<T, Allocator> const &rhs);
-            friend bool operator>=(sparse_array<T, Allocator> const &lhs, sparse_array<T, Allocator> const &rhs);
+            template <typename T_, class Allocator_> friend bool operator==(sparse_array<T_, Allocator_> const &lhs, sparse_array<T_, Allocator_> const &rhs);
+            template <typename T_, class Allocator_> friend bool operator!=(sparse_array<T_, Allocator_> const &lhs, sparse_array<T_, Allocator_> const &rhs);
+            template <typename T_, class Allocator_> friend bool operator<(sparse_array<T_, Allocator_> const &lhs, sparse_array<T_, Allocator_> const &rhs);
+            template <typename T_, class Allocator_> friend bool operator>(sparse_array<T_, Allocator_> const &lhs, sparse_array<T_, Allocator_> const &rhs);
+            template <typename T_, class Allocator_> friend bool operator<=(sparse_array<T_, Allocator_> const &lhs, sparse_array<T_, Allocator_> const &rhs);
+            template <typename T_, class Allocator_> friend bool operator>=(sparse_array<T_, Allocator_> const &lhs, sparse_array<T_, Allocator_> const &rhs);
         private:
             void maybe_resize(size_type pos) {
-                if (pos > base_t::size())
+                if (pos >= base_t::size())
                     base_t::resize(pos + 1);
             }
     };
+
+
+    template <typename T, typename = std::enable_if_t<!meta::is_optional_v<T>>>
+    sparse_array(size_t count, T) -> sparse_array<T>;
+
+    template <typename T,
+             typename Allocator = std::allocator<std::optional<T>>,
+             typename = std::enable_if_t<!meta::is_optional_v<T>>>
+    sparse_array(size_t count, T, Allocator) -> sparse_array<T>;
+
+    template <typename T>
+    sparse_array(size_t count, std::optional<T>) -> sparse_array<T>;
+
+    template <typename T, typename Allocator = std::allocator<std::optional<T>>>
+    sparse_array(size_t count, std::optional<T>, Allocator) -> sparse_array<T>;
+
+    template <typename InputIt,
+             typename T = typename std::iterator_traits<InputIt>::value_type, class Allocator = std::allocator<std::optional<T>>,
+             typename = meta::require_input_iterator<InputIt>,
+             typename = std::enable_if_t<!meta::is_optional_v<T>>
+    >
+    sparse_array(InputIt, InputIt) -> sparse_array<T, Allocator>;
+
+    template <typename InputIt,
+             typename T = typename std::iterator_traits<InputIt>::value_type, class Allocator = std::allocator<T>,
+             typename = meta::require_input_iterator<InputIt>
+    >
+    sparse_array(InputIt, InputIt) -> sparse_array<typename T::value_type, Allocator>;
+
+    template <typename InputIt,
+             typename T = typename std::iterator_traits<InputIt>::value_type, class Allocator = std::allocator<std::optional<T>>,
+             typename = meta::require_input_iterator<InputIt>,
+             typename = std::enable_if_t<!meta::is_optional_v<T>>
+    >
+    sparse_array(InputIt, InputIt, Allocator) -> sparse_array<T, Allocator>;
+
+    template <typename InputIt,
+             typename T = typename std::iterator_traits<InputIt>::value_type, class Allocator = std::allocator<T>,
+             typename = meta::require_input_iterator<InputIt>
+    >
+    sparse_array(InputIt, InputIt, Allocator) -> sparse_array<typename T::value_type, Allocator>;
+
+    template <typename T, class Allocator = std::allocator<std::optional<T>>>
+    sparse_array(std::initializer_list<T>, Allocator) -> sparse_array<T, Allocator>;
+
+    template <typename T, class Allocator = std::allocator<std::optional<T>>>
+    sparse_array(std::initializer_list<std::optional<T>>, Allocator) -> sparse_array<T, Allocator>;
+
+    template <typename T, class Allocator = std::allocator<std::optional<T>>>
+    sparse_array(std::initializer_list<T>) -> sparse_array<T, Allocator>;
+
+    template <typename T, class Allocator = std::allocator<std::optional<T>>>
+    sparse_array(std::initializer_list<std::optional<T>>) -> sparse_array<T, Allocator>;
 
     /**
     ** \name Non-member functions
