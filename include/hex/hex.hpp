6ea233fb4baf655092ed1b52a53436ad898c6889
Phantomas <phantomas@phantomas.xyz>
2021-12-11 17:32:00 +0100

    add: Add zip and izip pseudo-containers

--- include/hex/hex.hpp
+++ include/hex/hex.hpp
@@ -3,7 +3,7 @@
 **
 ** \author Phantomas <phantomas@phantomas.xyz>
 ** \date Created on: 2021-12-05 16:33
-** \date Last update: 2021-12-05 17:16
+** \date Last update: 2021-12-11 16:29
 */
 
 #ifndef HEX_HPP__
@@ -14,9 +14,14 @@
 #include "hex/entity_manager.hpp"
 #include "hex/system_registry.hpp"
 #include "hex/context.hpp"
+#include "hex/iterators/zip.hpp"
+#include "hex/utilities/indexer.hpp"
 
 namespace hex {
     using containers::sparse_array;
+    using iterators::zip;
+    using iterators::izip;
+    using utility::indexer;
 }
 
 #endif /* end of include guard: HEX_HPP__ */
---
+++ include/hex/utilities/indexer.hpp
@@ -0,0 +1,93 @@
+/**
+** \file indexer.hpp
+** \author Phantomas <phantomas@phantomas.xyz>
+** \date Created on: 2021-12-11 15:46
+** \date Last update: 2021-12-11 17:07
+*/
+
+#ifndef utility_indexer_hpp__
+#define utility_indexer_hpp__
+
+#include <cstddef> // std::size_t
+#include <iterator> // std::input_iterator_tag
+#include <limits> // std::numeric_limits
+#include <type_traits>
+
+namespace hex::utility {
+    class indexer_iterator {
+        public:
+            using value_type = std::size_t;
+            using reference = value_type;
+            using pointer = void;
+            using difference_type = std::ptrdiff_t;
+            using iterator_category = std::input_iterator_tag;
+
+
+            indexer_iterator() : _state{0}, _is_end(false) {}
+            indexer_iterator(bool is_end) : _state{0}, _is_end(is_end) {}
+
+            indexer_iterator(indexer_iterator const &) = default;
+            indexer_iterator(indexer_iterator &) noexcept = default;
+            ~indexer_iterator() = default;
+
+            indexer_iterator &operator=(indexer_iterator const &) = default;
+            indexer_iterator &operator=(indexer_iterator &&) noexcept = default;
+
+            indexer_iterator &operator++() noexcept {
+                _is_end = std::numeric_limits<std::size_t>::max() == _state;
+                ++_state;
+                return *this;
+            }
+
+            indexer_iterator operator++(int) noexcept {
+                auto tmp = *this;
+                ++(*this);
+                return tmp;
+            }
+
+            indexer_iterator operator+(difference_type i) {
+                auto tmp = *this;
+                tmp._state += i;
+                tmp._is_end = _is_end || (i < 0 ? _state < tmp._state : _state > tmp._state);
+
+                return tmp;
+            }
+
+            friend bool operator==(indexer_iterator const &lhs, indexer_iterator const &rhs) {
+                 return lhs._state == rhs._state || (lhs._is_end && rhs._is_end);
+            }
+
+            friend bool operator!=(indexer_iterator const &lhs, indexer_iterator const &rhs) {
+                return !(lhs == rhs);
+            }
+
+            value_type operator*() { return _state; }
+            value_type operator->() { return _state; }
+
+            void swap(indexer_iterator &oth) noexcept(std::is_nothrow_swappable_v<std::size_t> && std::is_nothrow_swappable_v<bool>) {
+                using std::swap;
+                swap(_state, oth._state);
+                swap(_is_end, oth._is_end);
+            }
+
+        private:
+            std::size_t _state;
+            bool _is_end;
+    };
+
+    void swap(indexer_iterator &lhs, indexer_iterator &rhs) {
+        lhs.swap(rhs);
+    }
+
+    struct indexer_t {
+        std::size_t size() const { return std::numeric_limits<std::size_t>::max(); }
+        indexer_iterator begin() { return {}; }
+        indexer_iterator end() { return {false}; }
+
+        indexer_iterator begin() const { return {}; }
+        indexer_iterator end() const { return {false}; }
+    };
+
+    static constexpr indexer_t indexer;
+}
+#endif /* end of include guard: utility_indexer_hpp__ */
